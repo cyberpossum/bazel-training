93181f412e98dd7a7909969522631f3961448dc0
cyberpossum <61364422+cyberpossum@users.noreply.github.com>
2020-08-16 22:07:39 +0300

    Fix shellcheck on test_functions.sh

--- test_functions.sh
+++ test_functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function fail {
     echo "${1}" >&2
     exit 1
@@ -8,5 +10,5 @@ function test_query {
     MD5SUM="${2:?MD5 sum is required}"
     ERRMSG="${3:?error message is required}"
     GREP="${4:-^.*$}"
-    [ "$(bazel query ${QUERY} 2>/dev/null|grep ${GREP}|sort|md5sum|cut -f1 -d' ')" == "${MD5SUM}" ] || fail "${ERRMSG}"
+    [ "$(bazel query "${QUERY}" 2>/dev/null|grep "${GREP}"|sort|md5sum|cut -f1 -d' ')" = "${MD5SUM}" ] || fail "${ERRMSG}"
 }
